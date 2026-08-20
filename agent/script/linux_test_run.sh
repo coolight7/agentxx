e7e75a43651c2b43fb2ed9ceabd64b8f279eac1b
coolight <2465045051@qq.com>
2026-08-20 14:38:30 +0800

    fix test

--- agent/script/linux_test_run.sh
+++ agent/script/linux_test_run.sh
@@ -10,4 +10,7 @@ if [[ $? -ne 0 ]]; then
     exit $?
 fi
 
-LD_LIBRARY_PATH=$build_dir/exec $build_dir/exec/agentxx_test
+# 测试依赖 cwd 下的 plugins/ 等资源 (与可执行文件同目录):
+# 无论从哪个目录运行脚本, 都切到可执行文件目录再运行
+cd "$build_dir/exec" || exit 1
+LD_LIBRARY_PATH=. ./agentxx_test
