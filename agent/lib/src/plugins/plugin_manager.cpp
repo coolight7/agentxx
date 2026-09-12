0bd79fe6ffa93cd7350e1e5bca10614b04f0206f
coolight <2465045051@qq.com>
2026-09-13 06:23:48 +0800

    --

--- agent/lib/src/plugins/plugin_manager.cpp
+++
@@ -1,3 +0,0 @@
-/// (DEPRECATED: plugin_manager.cpp 已拆分为 plugin_manager_lifecycle.cpp,
-/// plugin_manager_vtable.cpp, plugin_manager_adapters.cpp, plugin_manager_scheduler.cpp,
-/// plugin_manager_capability.cpp)
